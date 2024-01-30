e26fb98750cceb6f40168146f6ec6245b9dd35c6
marc-rigter <marcrigter@gmail.com>
2024-01-30 09:50:52 +0000

    tidy config

--- config/debug_autoregressive.py
+++
@@ -1,9 +0,0 @@
-from .debug import base, args_to_watch, logbase
-
-params = {
-        'diffusion_method': 'autoregressive',
-        'suite':'gym',
-        'horizon': 2, # train on sequences of two states
-        'rollout_steps': 100, # how far to rollout autoregressively
-}
-base.update(params)
